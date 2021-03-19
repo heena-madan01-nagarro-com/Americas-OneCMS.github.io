c159bc04fb69b93090e1193b4918d5a843b255b6
Anshul <anshul.jain01@nagarro.com>
2021-03-19 14:06:20 +0530

    OMTA-34 | Footer Component Base Commit

---
+++ core/src/main/java/com/assaabloy/americas/onecms/core/models/Footer.java
@@ -0,0 +1,42 @@
+/*
+ * ***********************************************************************
+ * ASSA ABLOY Americas Sites Project CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2021 ASSA ABLOY Americas Sites Project.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property
+ * of ASSA ABLOY Americas Sites Project and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to ASSA ABLOY Americas Sites Project
+ * and its suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from ASSA ABLOY Americas Sites Project.
+ * ***********************************************************************
+ */
+
+package com.assaabloy.americas.onecms.core.models;
+
+import java.util.List;
+import com.adobe.cq.export.json.ComponentExporter;
+import org.osgi.annotation.versioning.ConsumerType;
+
+
+/**
+ * Defines the {@code Footer} Sling Model used for the {@code onecms/components/content/footer} component.
+ * 
+ */
+@ConsumerType
+public interface Footer
+    extends ComponentExporter
+{
+
+
+    List<FooterColumn> getFooterColumns();
+
+    String getCopyrightLogo();
+
+    List<CopyrightLink> getCopyrightLinks();
+
+}
