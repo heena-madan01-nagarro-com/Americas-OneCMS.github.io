8972aa848f4aa4f487ada007932309588515aeac
Anshul <anshul.jain01@nagarro.com>
2021-03-26 21:21:22 +0530

    OMTA- 4 | Header Component | UI Package

--- ui.apps/aem-clientlib.js
+++ ui.apps/aem-clientlib.js
@@ -11,14 +11,17 @@ const {ClientLibrary, getFilesWithExtension, replaceFileContent} = require('./ae
 
 const NODE_MODULES_FOLDER = "node_modules";
 const CLIENTLIBS_ROOT_FOLDER = "./src/main/content/jcr_root/apps/onecms/clientlibs";
+const LIBRARY_TO_INCLUDE = "one-cms-library"; // Put Librarby name here which we want to include
 
 fse.readdirSync(NODE_MODULES_FOLDER).map(module => {
 	let clientlibName = module;
-	let modulePath = `./${NODE_MODULES_FOLDER}/${module}`;
-	let clientLib = new ClientLibrary(`${CLIENTLIBS_ROOT_FOLDER}/components`, clientlibName);
-	clientLib.categories = [`assa-abloy-onecms.${clientlibName}`];
-	clientLib.dependencies = '';
-	getFilesWithExtension(modulePath,".js").map(file => clientLib.jsFiles.push(`${modulePath}/${file}`));
-	getFilesWithExtension(modulePath,".css").map(file => clientLib.cssFiles.push(`${modulePath}/${file}`));
-	clientLib.create();
+	if(module == LIBRARY_TO_INCLUDE){
+		let modulePath = `./${NODE_MODULES_FOLDER}/${module}`;
+		let clientLib = new ClientLibrary(`${CLIENTLIBS_ROOT_FOLDER}/components/common`, clientlibName);
+		clientLib.categories = [`assa-abloy-onecms.${clientlibName}`];
+		clientLib.dependencies = '';
+		getFilesWithExtension(modulePath,".js").map(file => clientLib.jsFiles.push(`${modulePath}/${file}`));
+		getFilesWithExtension(modulePath,".css").map(file => clientLib.cssFiles.push(`${modulePath}/${file}`));
+		clientLib.create();
+	}
 });
